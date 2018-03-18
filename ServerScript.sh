dfea656479e6c9a1f638bc90c35e00842047a450
Humberto Chávez <humberto.chavez@gmail.com>
2018-03-17 21:49:02 -0700

    Create ServerScript.sh

---
+++ ServerScript.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Creación de un script en un servidor Linux como parte de las actividades del
+# laboratorio 3 de la materia de Utilerías y Herramientas de Software Libre de la
+# Maestría en Software Libre”
+#clear
+  cp -r /root/ /rootbkp
+  etc/init.d/apache2 stop
+  cp -r /var/www /home/wwwbkp
+  etc/init.d/apache2 restart
+  init 6
+exit 0
