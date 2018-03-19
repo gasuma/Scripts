0e053bb1c4459968d34a6f6f247c9599a603910c
Humberto Chávez <humberto.chavez@gmail.com>
2018-03-18 22:25:15 -0700

    Update ServerScript.sh

--- ServerScript.sh
+++ ServerScript.sh
@@ -3,7 +3,7 @@
 # Creación de un script en un servidor Linux como parte de las actividades del
 # laboratorio 3 de la materia de Utilerías y Herramientas de Software Libre de la
 # Maestría en Software Libre”
-#clear
+clear
   cp -r /root/ /rootbkp
   etc/init.d/apache2 stop
   cp -r /var/www /home/wwwbkp
